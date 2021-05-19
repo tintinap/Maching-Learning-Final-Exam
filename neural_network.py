55074b37ed47d5ada78ca126a2d95189234e037b
tintinap <31465850+tintinap@users.noreply.github.com>
2021-05-19 20:11:23 +0700

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -62,12 +62,15 @@ def update_weights(network, row, l_rate):
 # Train a network for a fixed number of epochs
 def train_network(network, train, l_rate, n_epoch, n_outputs):
 	for epoch in range(n_epoch):
+		sum_error = 0
 		for row in train:
 			outputs = forward_propagate(network, row)
 			expected = [0 for i in range(n_outputs)]
 			expected[row[-1]] = 1
+			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
 			backward_propagate_error(network, expected)
 			update_weights(network, row, l_rate)
+		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
  
 # Initialize a network
 def initialize_network(n_inputs, n_hidden, n_outputs):
